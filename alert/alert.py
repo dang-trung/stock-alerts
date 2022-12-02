a1b8916d0b9eebe7fea5970b9dd9319d7428ebb0
Trung Dang <dangtrung96@gmail.com>
2022-12-02 17:54:37 +0700

    Minor update.

--- alert/alert.py
+++ alert/alert.py
@@ -27,6 +27,7 @@ def _prep_hist(hist: dict) -> dict:
         hist.pop('s')
     else:
         print(hist)
+        return None  # no data
 
     rename_keys = {
         't': 'timestamp',
@@ -57,7 +58,40 @@ def _prep_hist(hist: dict) -> dict:
     return hist
 
 
-def get_latest_data(symbol: str) -> dict:
+def get_bar(symbol, closed_at: dt.datetime, timeframe: str = '1m') -> dict:
+    if timeframe == '1m':
+        from_ts = (closed_at - dt.timedelta(seconds=61)).timestamp()
+        print(closed_at - dt.timedelta(seconds=61))
+        to_ts = closed_at.timestamp()
+
+        return get_historical_data(
+            symbol=symbol, resolution='1', from_ts=from_ts, to_ts=to_ts
+        ) | {
+            'timeframe': timeframe
+        }
+    elif timeframe == '5m':
+        if closed_at.minute % 5 != 0:
+            closed_at += dt.timedelta(seconds=60 * (5 - (closed_at.minute % 5)))
+
+        from_ts = (closed_at - dt.timedelta(seconds=301)).timestamp()
+        to_ts = closed_at.timestamp()
+
+        hist = get_historical_data(
+            symbol=symbol, resolution='1', from_ts=from_ts, to_ts=to_ts
+        ) | {
+            'timeframe': timeframe
+        }
+        hist['timestamp'] = hist['timestamp'][0].replace(second=0)
+        hist['open'] = hist['open'][0]
+        hist['close'] = hist['close'][-1]
+        hist['high'] = max(hist['high'])
+        hist['low'] = min(hist['low'])
+        hist['vol'] = sum(hist['vol'])
+        
+        return hist
+
+
+def get_current_data(symbol: str) -> dict:
     now = dt.datetime.now().replace(microsecond=0)
     curr = get_historical_data(
         symbol=symbol,
@@ -65,7 +99,9 @@ def get_latest_data(symbol: str) -> dict:
         from_ts=now.replace(second=0).timestamp(),
         to_ts=now.timestamp()
     )
-    curr['timestamp'] = now
+    if curr is not None:
+        curr['timestamp'] = now
+
     return curr
 
 
@@ -75,8 +111,9 @@ def fetch_continuous_data(
     verbose: bool = True,
 ) -> None:
     while True:
-        curr = get_latest_data(symbol=symbol)
+        curr = get_current_data(symbol=symbol)
         if curr['close'] is not None:
             if verbose:
                 print(f"{symbol} | {curr['timestamp']} | {curr['close']}")
         time.sleep(interval)
+    
--- tests/test_data.py
+++ tests/test_data.py
@@ -15,13 +15,30 @@ class DataTestCase(unittest.TestCase):
         self.assertEqual(type(hist), dict)
         print(hist)
 
-    def test_get_latest_data(self):
-        hist = get_latest_data(symbol='VN30F1M')
-        self.assertEqual(type(hist), dict)
-        print(hist)
+    def test_get_current_data(self):
+        hist = get_current_data(symbol='VN30F1M')
+        if 9 <= dt.datetime.now().hour <= 14:
+            self.assertEqual(type(hist), dict)
+            print(hist)
+        else:
+            self.assertEqual(hist, None)  # no data after trading session
 
     def test_fetch_continuous_data(self):
         fetch_continuous_data(symbol='VN30F1M')
+
+    def test_get_bar(self):
+        hist = get_bar(
+            symbol='VN30F1M', closed_at=dt.datetime(2022, 12, 1, 14, 30)
+        )
+        self.assertEqual(hist['timestamp'].minute, 29)
+        print(hist)
+        hist = get_bar(
+            symbol='VN30F1M',
+            closed_at=dt.datetime(2022, 12, 1, 14, 30),
+            timeframe='5m'
+        )
+        self.assertEqual(hist['timestamp'].minute, 25)
+        print(hist)
 
 
 if __name__ == '__main__':
